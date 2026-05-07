11978a8aa7fddc4d67d6d5f49cba65f37c394e64
Mukesh Ranjan <mukeshranjan@disroot.org>
2026-05-07 12:09:03 +0000

    cleaned up github actions and python file

--- run_gemini.py
+++ run_gemini.py
@@ -25,7 +25,7 @@ commands = [
     f"export GEMINI_API_KEY=Q9A4AXWDH3V7K6GWMGWQ7TNW",
     f"export GOOGLE_GEMINI_BASE_URL=https://app.dataannotation.tech/api/llm_proxy/gemini",
     f"cd {PROJECT_FOLDER}",
-    "gemini -p 'Mske smart contract more efficient'"
+    "gemini -p 'add data validation'"
 ]
 
 for cmd in commands:
