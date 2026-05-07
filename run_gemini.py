0da0c8830f69a502192ea9d7ed0d0581bb881d4a
Mukesh Ranjan <mukeshranjan@disroot.org>
2026-05-07 11:27:01 +0000

    changed the repository to smart contract

--- run_gemini.py
+++ run_gemini.py
@@ -25,7 +25,7 @@ commands = [
     f"export GEMINI_API_KEY=Q9A4AXWDH3V7K6GWMGWQ7TNW",
     f"export GOOGLE_GEMINI_BASE_URL=https://app.dataannotation.tech/api/llm_proxy/gemini",
     f"cd {PROJECT_FOLDER}",
-    "gemini -p 'Add cloud coputing'"
+    "gemini -p 'Mske smart contract more efficient'"
 ]
 
 for cmd in commands:
