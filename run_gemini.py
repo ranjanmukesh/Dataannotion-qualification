e8835de95d5a59d01f0cd45ddddb313fc8715b8b
Mukesh Ranjan <mukeshranjan@disroot.org>
2026-05-08 12:18:20 +0000

    modifies prompt

--- run_gemini.py
+++ run_gemini.py
@@ -28,7 +28,7 @@ commands = [
     f"export TERM=xterm-256color",
     f"export COLORTERM=truecolor",
     f"cd {PROJECT_FOLDER}",
-    "gemini --approval-mode=auto_edit -p 'make the app configurable'"
+    "gemini --approval-mode=auto_edit -p 'inxrease the usaability of the app'"
 ]
 
 for cmd in commands:
