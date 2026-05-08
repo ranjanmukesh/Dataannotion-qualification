95d9e3ecc9cb0ac21484c7b6da3ee2347c80dbb6
Mukesh Ranjan <mukeshranjan@disroot.org>
2026-05-08 04:12:25 +0000

    modified prompt

--- run_gemini.py
+++ run_gemini.py
@@ -28,7 +28,7 @@ commands = [
     f"export TERM=xterm-256color",
     f"export COLORTERM=truecolor",
     f"cd {PROJECT_FOLDER}",
-    "gemini --approval-mode=auto_edit -p 'Add async to make app faster and make the app backword compatible to python 2.7'"
+    "gemini --approval-mode=auto_edit -p 'Add cloud computing feature'"
 ]
 
 for cmd in commands:
