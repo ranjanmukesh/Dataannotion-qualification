f98daa8b1f5e525c9555955d8a74f6331c91eaac
Mukesh Ranjan <mukeshranjan@disroot.org>
2026-05-17 02:08:46 +0000

    modified prompt

--- run_gemini.py
+++ run_gemini.py
@@ -59,7 +59,7 @@ commands = [
     "which rg || echo 'rg not found in PATH'",
     "rg --version && echo '✅ ripgrep is working' || echo '❌ ripgrep still missing'",
     "echo '**********************'",
-    f"gemini --approval-mode=auto_edit -p '{USER_PROMPT} . Start your respinse with <EOR> and end with <EOR>'"
+    f"gemini --approval-mode=auto_edit -p '{USER_PROMPT} . Start your respinse with GEMINI:'"
 ]
 
 for cmd in commands:
